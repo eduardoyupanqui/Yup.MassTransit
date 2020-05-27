c07bb9ac2aec1533eda984d32df3d2ed14832f71
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2020-05-27 13:30:35 -0500

    Extensions para ReceivedJobEndpoint

---
+++ src/Yup.MassTransit/Extensions/IRabbitMqBusFactoryConfiguratorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Yup.MassTransit.Jobs;
+using Yup.MassTransit.Jobs.Commands;
+using Yup.MassTransit.Jobs.Consumers;
+using MassTransit;
+using MassTransit.RabbitMqTransport;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Yup.MassTransit.Jobs.Events;
+
+namespace Yup.MassTransit.Extensions
+{
+    public static class IRabbitMqBusFactoryConfiguratorExtensions
+    {
+        public static void ReceivedJobEndpoint<T>(this IRabbitMqBusFactoryConfigurator cfg, IServiceProvider provider, string host = null)
+            where T : BaseExecutor
+        {
+            EndpointConvention.Map<JobStarted>(new Uri($"queue:{typeof(JobEvent).Name.ToUnderscoreCase()}"));
+            EndpointConvention.Map<JobTaskCompleted>(new Uri($"queue:{typeof(JobEvent).Name.ToUnderscoreCase()}"));
+            EndpointConvention.Map<JobCompleted>(new Uri($"queue:{typeof(JobEvent).Name.ToUnderscoreCase()}"));
+            EndpointConvention.Map<JobFailed>(new Uri($"queue:{typeof(JobEvent).Name.ToUnderscoreCase()}"));
+            cfg.ReceiveEndpoint(typeof(T).Name.ToUnderscoreCase().ToConcatHost(host), ep =>
+            {
+                ep.Consumer<JobConsumer<T>>(provider);
+            });
+        }
+    }
+}
---
+++ src/Yup.MassTransit/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yup.MassTransit.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToUnderscoreCase(this string str)
+        {
+            return string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToLower();
+        }
+
+        public static string ToConcatHost(this string queue, string host)
+        {
+            return string.IsNullOrEmpty(host) ?
+                queue :
+                host + "_" + queue;
+        }
+    }
+}
